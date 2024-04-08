ce51099f71ccacc96f920e81f6866adff946800e
granttemplin <60511652+grantemplin@users.noreply.github.com>
2024-04-08 11:30:28 -0400

    Going to make the filter into a state machine to deal with beginning/end cases, which add complexity

--- VHDL/bilinear_interpolation.vhd
+++ VHDL/bilinear_interpolation.vhd
@@ -1,3 +1,11 @@
+-- -------- INPUT INFO: ----------------
+-- Reset should be done before each new image is created
+-- First inputs need to be original image dimensions, then new image dimensions 
+-- Stream in RGB pixels in raster fashion, a two-line buffer will be used to hold pixels
+
+
+
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 use IEEE.NUMERIC_STD.ALL; --package needed for SIGNED
@@ -13,26 +21,47 @@ ENTITY bilinear_interpolation IS
             green_out: OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
             blue_out: OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
             -- Control signals
-            line_width: IN STD_LOGIC_VECTOR(10 DOWNTO 0); -- up to 2048 pixels per line, more than sufficient for 1080p
-            loading: IN STD_LOGIC; -- filter needs two full lines before it can perform calculations 
+            input_width: IN STD_LOGIC_VECTOR(10 DOWNTO 0); -- up to 2048 pixels per line, more than sufficient for 1080p
+            input_height: IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+            output_width: IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+            output_height:  IN STD_LOGIC_VECTOR(10 DOWNTO 0);
             reset: IN STD_LOGIC;
             );
 END bilinear_interpolation;
 ----------------------------------------------------------------------------------------------------------------------------------
 ARCHITECTURE Behavioral OF bilinear_interpolation IS
+    constant max_line_length : INTEGER := 2048;
 -- type declarations
-    TYPE single_line IS ARRAY (0 to 4095) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
-    TYPE line_buffer IS ARRAY (0 TO 1) OF single_line;
+    TYPE single_line IS ARRAY (0 TO max_line_length*3-1) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+    TYPE line_buffer IS ARRAY (0 TO 2) OF single_line;
+    TYPE state IS (waiting, initial_load, process_load_next, final_processing);
 -- signal declarations 
     SIGNAL red_buffer : line_buffer;
     SIGNAL green_buffer : line_buffer;
     SIGNAL blue_buffer: line_buffer;
-    SIGNAL counter : INTEGER;
+    SIGNAL next_line_ready : STD_LOGIC; -- can calculations take place yet (true or false) 
+    SIGNAL red_int, green_int, blue_int : unsigned(7 downto 0); -- internal signals for pixels 
+    SIGNAL height : STD_LOGIC_VECTOR(10 DOWNTO 0);
+    SIGNAL width : STD_LOGIC_VECTOR(10 DOWNTO 0);
+    SIGNAL current_state : state;
+-- variable declarations 
+    VARIABLE pixel_counter : INTEGER; -- where you are in the line
+    VARIABLE line_counter : INTEGER; -- the line you are on itself 
 BEGIN
     PROCESS(clk, rst)
     BEGIN
+    -- Obtain the internal signals 
+        red_int <= unsigned(red_in);
+        green_int <= unsigned(green_in);
+        blue_int <= unsigned(blue_in);
+        height <= unsigned(input_height); 
+        width <= unsigned(input_width); 
+    -- Process the inputs 
         IF reset = '1' THEN
-            counter := 0; 
+            pixel_counter := 0; 
+            line_counter := 0; 
+            loading <= '1' -- will need to load in new pixel values upon a reset 
+            current_state := waiting; -- waiting for pixels to be loaded in  
             FOR i IN line_buffer'range
                 FOR j IN single_line'range
                     red_buffer(i)(j) <= (OTHERS => '0')
@@ -41,12 +70,9 @@ BEGIN
                 END LOOP;
             END LOOP;
         ELSIF rising_edge(clk) THEN
-            IF loading = '1' THEN
-                -- don't yet have full buffers
-            ELSE
-                -- full buffers ready for filtering 
-            END IF; 
-        END IF;
+            CASE(current_state) IS 
+            END CASE;
+        END IF; -- end of rising edge process 
     END PROCESS
 --combinational
 END Behavioral
